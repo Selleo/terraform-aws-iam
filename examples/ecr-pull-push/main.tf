e1dcf72bf5ce2c75634b9c6428157dcc360bf418
Mateusz Wieczorek <mateusz.wu@gmail.com>
2021-12-29 22:44:58 +0100

    rename 'ecr' module to 'ecr-pull-push'

--- examples/ecr-pull-push/main.tf
+++ examples/ecr-pull-push/main.tf
@@ -49,7 +49,7 @@ output "iam_push_user_secret" {
 }
 
 module "aws_iam_ecr_policy" {
-  source      = "../../modules/ecr"
+  source      = "../../modules/ecr-pull-push"
   name_prefix = "aws-iam"
   ecr_arn     = aws_ecr_repository.this.arn
   pull_users  = [aws_iam_user.pull.name]
