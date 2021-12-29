e53db7064896d14b2adec30a529c1ea6f0f3a1c7
Mateusz Wieczorek <mateusz.wu@gmail.com>
2021-12-29 20:51:45 +0100

    fix spelling mistakes and output variable formatting

--- examples/cloudfront/main.tf
+++ examples/cloudfront/main.tf
@@ -52,11 +52,11 @@ resource "aws_cloudfront_distribution" "example_cloudfront" {
   }
 }
 
-output "CLOUDFRONT_domain_name" {
+output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.example_cloudfront.domain_name
 }
 
-output "CLOUDFRONT_distribution_id" {
+output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.example_cloudfront.id
 }
 
