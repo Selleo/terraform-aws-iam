ff847452051b992270edf703aeb5da979d519263
qbart <wojtowicz.bartlomiej@gmail.com>
2022-04-12 15:50:39 +0200

    chore: Update path module for SSM

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,9 +1,9 @@
 locals {
-  ssm_ec2_policy = templatefile("./tpl/ssm_policy.json", {
+  ssm_ec2_policy = templatefile("${path.module}/tpl/ssm_policy.json", {
     kms_arn = var.kms_arn
   })
 
-  ssm_access_policy = templatefile("./tpl/ssm_access_policy.json", {
+  ssm_access_policy = templatefile("${path.module}/tpl/ssm_access_policy.json", {
     kms_arn       = var.kms_arn
     ec2_tag_key   = var.ssm_tag_key
     ec2_tag_value = var.ssm_tag_value
